85617353fb881bf5d2f99fbd8b3fcf689f0300d3
Zigzagzen7 <alexandre.marcil@gmail.com>
2019-03-22 18:38:37 -0400

    id to words

--- generate.py
+++ generate.py
@@ -95,14 +95,45 @@ argsdict['code_file'] = sys.argv[0]
 #     i += 1
 # experiment_path = experiment_path + "_" + str(i)
 
+# HELPER FUNCTIONS
+def _read_words(filename):
+    with open(filename, "r") as f:
+      return f.read().replace("\n", "<eos>").split()
 
+def _build_vocab(filename):
+    data = _read_words(filename)
 
-# # LOAD DATA
-# print('Loading data from '+args.data)
-# raw_data = ptb_raw_data(data_path=args.data)
-# train_data, valid_data, test_data, word_to_id, id_2_word = raw_data
-# vocab_size = len(word_to_id)
-# print('  vocabulary size: {}'.format(vocab_size))
+    counter = collections.Counter(data)
+    count_pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+
+    words, _ = list(zip(*count_pairs))
+    word_to_id = dict(zip(words, range(len(words))))
+    id_to_word = dict((v, k) for k, v in word_to_id.items())
+
+    return word_to_id, id_to_word
+
+def _file_to_word_ids(filename, word_to_id):
+    data = _read_words(filename)
+    return [word_to_id[word] for word in data if word in word_to_id]
+
+# Processes the raw data from text files
+def ptb_raw_data(data_path=None, prefix="ptb"):
+    train_path = os.path.join(data_path, prefix + ".train.txt")
+    valid_path = os.path.join(data_path, prefix + ".valid.txt")
+    test_path = os.path.join(data_path, prefix + ".test.txt")
+
+    word_to_id, id_2_word = _build_vocab(train_path)
+    train_data = _file_to_word_ids(train_path, word_to_id)
+    valid_data = _file_to_word_ids(valid_path, word_to_id)
+    test_data = _file_to_word_ids(test_path, word_to_id)
+    return train_data, valid_data, test_data, word_to_id, id_2_word
+
+# LOAD DATA
+print('Loading data from '+args.data)
+raw_data = ptb_raw_data(data_path=args.data)
+train_data, valid_data, test_data, word_to_id, id_2_word = raw_data
+vocab_size = len(word_to_id)
+print('  vocabulary size: {}'.format(vocab_size))
 
 
 
@@ -110,12 +141,12 @@ argsdict['code_file'] = sys.argv[0]
 if args.model == 'RNN':
     model = RNN(emb_size=args.emb_size, hidden_size=args.hidden_size,
                 seq_len=args.seq_len, batch_size=args.batch_size,
-                vocab_size=10000, num_layers=args.num_layers,
+                vocab_size=vocab_size, num_layers=args.num_layers,
                 dp_keep_prob=args.dp_keep_prob)
 elif args.model == 'GRU':
     model = GRU(emb_size=args.emb_size, hidden_size=args.hidden_size,
                 seq_len=args.seq_len, batch_size=args.batch_size,
-                vocab_size=10000, num_layers=args.num_layers,
+                vocab_size=vocab_size, num_layers=args.num_layers,
                 dp_keep_prob=args.dp_keep_prob)
 
 
