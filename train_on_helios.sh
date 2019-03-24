f40f86b272fb5e5bad7d3ce4c891f8f24fb1d0ea
user name <alexandre.marcil@gmail.com>
2019-03-24 11:48:12 -0400

    last GRU results emb size

--- train_on_helios.sh
+++ train_on_helios.sh
@@ -8,6 +8,7 @@
 #s_exec python ptb-lm.py --model=GRU --optimizer=SGD --initial_lr=10 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35
 #s_exec python ptb-lm.py --model=GRU --optimizer=ADAM --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35
 
+
 # dropout = 1.0 / 0.65 / 0.5   (0.35 default) 
 #s_exec python ptb-lm.py --model=RNN --optimizer=ADAM --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=1.0
 #s_exec python ptb-lm.py --model=RNN --optimizer=ADAM --initial_lr=0.0001 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.65
@@ -34,7 +35,13 @@
 
 #GRU
 #s_exec python ptb-lm.py --model=GRU --optimizer=SGD_LR_SCHEDULE --initial_lr=10 --batch_size=100 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35
-s_exec python ptb-lm.py --model=GRU --optimizer=SGD_LR_SCHEDULE --initial_lr=10 --batch_size=15 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35
+#s_exec python ptb-lm.py --model=GRU --optimizer=SGD_LR_SCHEDULE --initial_lr=10 --batch_size=15 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35
+
+#s_exec python ptb-lm.py --model=GRU --optimizer=SGD_LR_SCHEDULE --initial_lr=10 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --emb_size=1000
+#s_exec python ptb-lm.py --model=GRU --optimizer=SGD_LR_SCHEDULE --initial_lr=10 --batch_size=20 --seq_len=35 --hidden_size=1500 --num_layers=2 --dp_keep_prob=0.35 --emb_size=400
+
+#Trans
+s_exec python ptb-lm.py --model=TRANSFORMER --optimizer=SGD_LR_SCHEDULE --initial_lr=20 --batch_size=128 --seq_len=35 --hidden_size=512 --num_layers=6 --dp_keep_prob=0.9 --emb_size=1000
 
 
 
